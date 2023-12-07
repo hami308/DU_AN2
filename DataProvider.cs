9ad9d6870c8e5330b971227477aa17554f9693ba
Hà My <145450102+hami308@users.noreply.github.com>
2023-12-07 21:41:40 +0700

    Add files via upload

---
+++ DataProvider.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiaoDien_qlpks.DAO
+{
+    public class DataProvider
+    {
+        private string connectionSTR = @"Data Source=LAPTOP-JKMABAVK\TESTSQL;Initial Catalog=QUANLYPHONGKHACHSAN;Integrated Security=True;TrustServerCertificate=true;";
+        public DataTable ExecuteQuery(string query)
+        {
+            SqlConnection connection = new SqlConnection(connectionSTR);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            DataTable data = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(data);
+            connection.Close();
+            return data;
+        }
+
+        public SqlDataReader ExecuteReader(string query)
+        {
+            SqlConnection connection = new SqlConnection(connectionSTR);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+        }
+        public int GetIdentity(string query)
+        {
+            int identity = -1;
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        identity = Convert.ToInt32(command.ExecuteScalar());
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi");
+                    }
+                }
+            }
+
+            return identity;
+        }
+
+        public bool Kiemtra( string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+               
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
+            }
+        }
+    }
+
+    }
